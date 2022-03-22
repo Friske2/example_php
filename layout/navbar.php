09e94f1b07530d1856eb6c9255e34786bc45d29d
jellopy <parnupat.j@gmail.com>
2022-03-22 11:54:53 +0700

    use app name

--- layout/navbar.php
+++ layout/navbar.php
@@ -1,17 +1,16 @@
-
+<?php define("APP_NAME", "sandbox"); ?>
 <header class="mb-auto">
     <div>
       <h3 class="float-md-start mb-0">Cover</h3>
       <nav id="app-navbar" class="nav nav-masthead justify-content-center float-md-end">
-        <a class="nav-link" id="index" aria-current="page" href="/sandbox">Home</a>
-        <a class="nav-link" id="features" href="/sandbox/Features.php">Features</a>
-        <a class="nav-link" id="contact" href="/sandbox/Contact.php">Contact</a>
+        <a class="nav-link" id="index" aria-current="page" href="/<?php  echo APP_NAME ?>">Home</a>
+        <a class="nav-link" id="features" href="/<?php  echo APP_NAME ?>/Features.php">Features</a>
+        <a class="nav-link" id="contact" href="/<?php  echo APP_NAME ?>/Contact.php">Contact</a>
       </nav>
     </div>
   </header>
-
 <script>
-  const APP_NAME = 'sandbox'
+  const APP_NAME = '<?php  echo APP_NAME ?>'
   const pathName = function() {
     const { pathname } = new URL(location.href)
     let path = pathname.replace('/'+APP_NAME,'').replace('.php','').replace('/','')
